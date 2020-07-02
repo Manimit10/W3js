e202497876023fac81e2e4e2ac6c16430a49b465
ManiMIT10 <m.tahriri.z@gmail.com>
2020-07-02 12:30:53 +0200

    Q1 finished

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const myDate = new Date();
 const curr_day = myDate.getDay();
 console.log(`Today is: ${d_names[curr_day]}`);
 
-const testday = new Date('March 13, 8 16:20');
-console.log(myDate.getHours());
-console.log(testday.getHours().toLocaleTimeString());
-console.log(testday.getFullYear());
+console.log(
+  `${myDate.toLocaleString('en-US', {
+    hour: 'numeric',
+    hour12: true,
+  })} : ${myDate.getMinutes()} : ${myDate.getSeconds()}`
+);
+
+//This is my test
+// const testday = new Date('March 13, 8 16:20');
+// console.log(myDate.getHours());
+// console.log(testday.getHours());
+// console.log(testday.getFullYear());
