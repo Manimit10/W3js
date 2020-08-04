c2a7fb0a857a870b28b3203cddbc8d0a92e54bf5
ManiMIT10 <m.tahriri.z@gmail.com>
2020-08-04 16:11:49 +0200

    question 22 added

--- q20-30.js
+++ q20-30.js
@@ -1,3 +1,4 @@
+//question 21
 function addPy() {
   const str = document.getElementById('string').value;
   const output = document.getElementById('outStr');
@@ -9,4 +10,20 @@ function addPy() {
     outStr = 'Py' + str;
     output.innerHTML = `<p>${outStr}</p>`;
   }
+}
+
+// Question 22
+function deleteChar() {
+  const word = document.getElementById('word').value;
+  let position = document.getElementById('position').value;
+  const newStr = document.getElementById('newStr');
+  position = parseInt(position);
+  position--;
+
+  const arStr = Array.from(word);
+
+  arStr.splice(position, 1);
+  const res = arStr.join('');
+
+  newStr.innerHTML = `<p>You will have <em>${res}</em></p>`;
 }
