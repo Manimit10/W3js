c14b446147c4025ecb34100ff49b8d5ce0e827f0
ManiMIT10 <m.tahriri.z@gmail.com>
2020-08-15 00:13:43 +0200

    question 1 and 2

--- object1.js
+++ object1.js
@@ -1,8 +1,23 @@
-const student = {
+const Student = {
   name: 'David Rayy',
   sclass: 'VI',
-  rillno: 12,
+  rollno: 12,
 };
+
+// question 1
 function firstObj() {
-  console.log(student);
+  const obj = document.getElementById('obj1');
+
+  obj.innerHTML = `<p>name is <b>${Student.name}</b></p>
+                    <p>class is <b>${Student.sclass}</b></p>
+                    <p>group is <b>${Student.rollno}</b></p>`;
+}
+// question 2
+function delObj() {
+  delete Student.rollno;
+  const obj = document.getElementById('obj2');
+  console.log(Student);
+  obj.innerHTML = `<p>name is <b>${Student.name}</b></p>
+                    <p>class is <b>${Student.sclass}</b></p>
+                    <p>group is <b>${Student.rollno}</b></p>`;
 }
