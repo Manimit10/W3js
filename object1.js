d27bac1cbe8772857f2f953568767c5d6d64b5cd
ManiMIT10 <m.tahriri.z@gmail.com>
2020-08-16 13:16:46 +0200

    question 3 added

--- object1.js
+++ object1.js
@@ -4,6 +4,15 @@ const Student = {
   rollno: 12,
 };
 
+Object.size = function (obj) {
+  var size = 0,
+    key;
+  for (key in obj) {
+    if (obj.hasOwnProperty(key)) size++;
+  }
+  return size;
+};
+
 // question 1
 function firstObj() {
   const obj = document.getElementById('obj1');
@@ -20,4 +29,10 @@ function delObj() {
   obj.innerHTML = `<p>name is <b>${Student.name}</b></p>
                     <p>class is <b>${Student.sclass}</b></p>
                     <p>group is <b>${Student.rollno}</b></p>`;
+}
+
+// Question 3
+function obj3() {
+  const obj3 = document.getElementById('obj3');
+  obj3.innerHTML = `<p>The object has <b>${Object.size(Student)}</b> members.</p>`;
 }
